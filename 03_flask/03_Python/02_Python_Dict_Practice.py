b4f7ffe48fd207e630879bf43c65750e19566f03
Chanwoo Nam <chanwoo.1993n@gmail.com>
2019-10-28 17:18:42 +0900

    00 Django intro | 191028

---
+++ 03_flask/03_Python/02_Python_Dict_Practice.py
@@ -0,0 +1,96 @@
+'''
+Python dictionary 연습 문제
+'''
+
+# 1. 평균을 구하시오.
+score = {
+    '수학': 80,
+    '국어': 90,
+    '음악': 100
+}
+
+# 아래에 코드를 작성해 주세요.
+print('==== Q1 ====')
+
+total_score = 0
+for subject_score in score.values():
+  total_score += subject_score
+  
+result = total_score / len(score.keys())
+print(result)
+
+# 2. 반 평균을 구하시오. -> 전체 평균
+scores = {
+    '민승': {
+        '수학': 80,
+        '국어': 90,
+        '음악': 100
+    },
+    '건희': {
+        '수학': 80,
+        '국어': 90,
+        '음악': 100
+    }
+}
+
+# 아래에 코드를 작성해 주세요.
+print('==== Q2 ====')
+
+## 나 
+
+total_score = 0
+persons = len(scores.keys())  # 사람 수 
+
+for person in scores.keys():
+  for subject_score in scores[person].values():
+    total_score += subject_score
+  result = total_score / len(scores[person].values())
+  print(total_score)
+  print(result)
+  result = 0
+  total_score = 0
+
+## 강사님
+
+
+# 3. 도시별 최근 3일의 온도입니다.
+cities = {
+    '서울': [-6, -10, 5],
+    '대전': [-3, -5, 2],
+    '광주': [0, -2, 10],
+    '부산': [2, -2, 9],
+}
+
+# 3-1. 도시별 최근 3일의 온도 평균은?
+
+# 아래에 코드를 작성해 주세요.
+print('==== Q3-1 ====')
+'''
+출력 예시)
+서울 : 평균온도
+대전 : 평균온도
+광주 : 평균온도
+부산 : 평균온도
+'''
+
+tt = 0
+for city, temp in cities.items():
+  print(f'{city} :  {sum(temp) / len(temp)}')
+  
+# 3-2. 도시 중에 최근 3일 중에 가장 추웠던 곳, 가장 더웠던 곳은?
+
+# 아래에 코드를 작성해 주세요.
+print('==== Q3-2 ====')
+temp_min= []
+for city, temp in cities.items():
+  temp_min = min(temp)
+  temp_max = max(temp)
+
+  vv = {'city' : city, 'min' : temp_min, 'max' : temp_max}
+  print(vv)
+
+
+# 3-3. 위에서 서울은 영상 2도였던 적이 있나요?
+
+# 아래에 코드를 작성해 주세요.
+print('==== Q3-3 ====')
--- 03_flask/app.py
+++ 03_flask/app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import random
+import requests
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -46,6 +49,73 @@ def cube(number):
 def movies():
   movie_list = ['82년생 김지영', '조커', '엔드게임', '궁예']
   return render_template('movies.html', movies=movie_list)
+
+# ping : 사용자로부터 입력을 받을 Form 페이지를 넘겨준다. 
+@app.route('/ping')
+def ping():
+  return render_template('ping.html')
+
+@app.route('/pong')
+def pong():
+  user_name = request.args.get('user_name')
+  return render_template('pong.html', user_name= user_name)
+
+# fake naver
+@app.route('/naver')
+def naver():
+  return render_template('naver.html')
+
+# fake google
+@app.route('/google')
+def google():
+  return render_template('google.html')
+
+# vonvon
+# 사용자로부터 이름을 입력받을 Form 페이지
+@app.route('/vonvon')
+def vonvon():
+  return render_template('vonvon.html')
+
+# 전달받은 이름을 기준으로 넘겨줄 각종 정보를 가공해서 돌려주는 (응답)로직!
+@app.route('/godmademe')
+def godmademe():
+  # 1. 사용자가 입력한 데이터를 가져온다. 
+  name =  request.args.get('user_name')
+  # 2. 사용자에게 보여줄 여러가지 재밌는 특성들 리스트를 만든다. 
+  first_list = ['잘생김', '못생김', '많이 못생김', '그냥 존잘..', '다비드 조각상', '오징어', '사람인가...']
+  second_list = ['순수함', '변태', '착함', '바보', '천재']
+  third_list = ['허세', '물욕', '식욕', '수면욕', ]
+  # 3. 리스트에서 랜덤으로 하나씩을 선택한다. 
+  first = random.choice(first_list)
+  second = random.choice(second_list)
+  third = random.choice(third_list)
+  # 4. 가공한 정보를 템플릿에 담아서 사용자에게 보여준다. 
+  return render_template('godmademe.html', name_html=name, fist_html=first, second_html=second, third_html=third)
+
+
+
+
+# http://artii.herokuapp.com/
+# 1. 사용자로부터 임의의 텍스트를 입력받아서, 아스키 아트로 변환해서 돌려준다.
+# 이때, 아스키 포트는 랜덤으로 하나를 지정해서 변환한다. 
+@app.route('/artii')
+def artii():
+  return render_template('artii.html')
+
+@app.route('/artii_result')
+def artii_result():
+  # 1. 사용자가 입력한 Form 데이터를 가져온다.
+  word = request.args.get('word')
+  # 2. ARTII API를 요청을 보내서, 응답 결과를 변수에 담는다. (폰트 정보들)
+  fonts = requests.get('http://artii.herokuapp.com/fonts_list').text
+  # 3. 가져온 폰트들을 리스트 형태로 바꾼다.
+  fonts = fonts.split('\n')
+  # 4. 폰트 하나를 랜덤으로 선택한다.
+  font = random.choice(fonts)
+  # 5. 사용자가 입력한 단어와 랜덤으로 선택한 폰트 정보를 담아서 API에게 요청한다.
+  result = requests.get(f'http://artii.herokuapp.com/make?text={word}&font={font}').text
+  # 6. 최종 결과물을 사용자에게 돌려준다.
+  return render_template('artii_result.html', result_html = result)
 
 
 # end of file !!!!!
